f64fb3ba2d15976052ba9e3877f7cb3c2261e2d6
田若星 <35066603+shiverking@users.noreply.github.com>
2021-05-12 01:06:50 +0800

    Delete V3__ADD NEW TAble JOB.sql

--- src/main/resources/db/migration/V3__ADD NEW TAble JOB.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `jobsite`.`job`
-ADD COLUMN `skill` varchar(255) NULL COMMENT '所需要的技术' AFTER `required`,
-ADD COLUMN `work_time` int NULL COMMENT '每天工作时间' AFTER `skill`,
-ADD COLUMN `position` varchar(255) NULL COMMENT '职位' AFTER `work_time`,
-ADD COLUMN `job_statue` int(255) NOT NULL COMMENT '表示该职位是否处于开启招聘，0为关闭，1为开启' AFTER `position`;
